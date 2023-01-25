6ee092e6a31d93a861e155f43c1fe35c3f633770
Paul Obinna Madu <88602720+paulphysio@users.noreply.github.com>
2023-01-25 01:59:26 +0100

    Update query-c.sql

--- Assignment/Assignment1/task4/query-c.sql
+++ Assignment/Assignment1/task4/query-c.sql
@@ -1,5 +1,5 @@
 drop table newdate;
-create table pollution_db2.NEWDATE(`Date Time` datetime, `PM2.5` text, `VPM2.5` text, `location` text);
+create table pollution_db2.NEWDATE(`Date Time` datetime, `PM2.5` double, `VPM2.5` double, `location` text);
 ALTER TABLE NEWDATE
 MODIFY COLUMN `Date Time` DATETIME;
 INSERT INTO pollution_db2.newdate select date_format(`Date Time`,'2019-%m-%d 08:%m:%S'), `PM2.5`, `VPM2.5`, `location id` FROM pollution_db2.readings where `Date Time` between '2010-01-01' and '2019-12-31';
