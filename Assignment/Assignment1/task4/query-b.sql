c8e05e3fb3721ca8e9cf53d97471a5085eca30de
Paul Obinna Madu <88602720+paulphysio@users.noreply.github.com>
2023-01-25 23:22:25 +0100

    Update query-b.sql

--- Assignment/Assignment1/task4/query-b.sql
+++ Assignment/Assignment1/task4/query-b.sql
@@ -1,24 +1,25 @@
-create table NEWDATE(`Date Time` datetime, `PM2.5` text, `VPM2.5` text, `location` text);
-ALTER TABLE NEWDATE
-MODIFY COLUMN `Date Time` DATETIME;
-INSERT INTO newdate select date_format(`Date Time`,'2019-%m-%d 08:%m:%S'), `PM2.5`, `VPM2.5`, `location id` FROM pollution_db2.readings where `Date Time` between '2019-01-01' and '2019-12-31';
+DROP TABLE newTABLE;
+create table newtable(`Date Time` datetime, `PM2.5` text, `VPM2.5` text, `location id` text);
 
-SELECT AVG(`PM2.5`), AVG(`VPM2.5`) FROM newdate where location= 'AURN Bristol Centre';
-SELECT AVG(`PM2.5`), AVG(`VPM2.5`) FROM newdate where location= 'Brislington Depot';
-SELECT AVG(`PM2.5`), AVG(`VPM2.5`) FROM newdate where location= 'Rupert Street';
-SELECT AVG(`PM2.5`), AVG(`VPM2.5`) FROM newdate where location= 'IKEA M32';
-SELECT AVG(`PM2.5`), AVG(`VPM2.5`) FROM newdate where location= 'Old Market';
-SELECT AVG(`PM2.5`), AVG(`VPM2.5`) FROM newdate where location= 'Parson Street School';
-SELECT AVG(`PM2.5`), AVG(`VPM2.5`) FROM newdate where location= 'Temple Meads Station';
-SELECT AVG(`PM2.5`), AVG(`VPM2.5`) FROM newdate where location= 'Wells Road';
-SELECT AVG(`PM2.5`), AVG(`VPM2.5`) FROM newdate where location= 'Trailer Portway P&R';
-SELECT AVG(`PM2.5`), AVG(`VPM2.5`) FROM newdate where location= 'Newfoundland Road Police Station';
-SELECT AVG(`PM2.5`), AVG(`VPM2.5`) FROM newdate where location= "Shiner's Garage";
-SELECT AVG(`PM2.5`), AVG(`VPM2.5`) FROM newdate where location= 'AURN St Pauls';
-SELECT AVG(`PM2.5`), AVG(`VPM2.5`) FROM newdate where location= 'Bath Road';
-SELECT AVG(`PM2.5`), AVG(`VPM2.5`) FROM newdate where location= 'Cheltenham Road \ Station Road';
-SELECT AVG(`PM2.5`), AVG(`VPM2.5`) FROM newdate where location= 'Fishponds Road';
-SELECT AVG(`PM2.5`), AVG(`VPM2.5`) FROM newdate where location= 'CREATE Centre Roof';
-SELECT AVG(`PM2.5`), AVG(`VPM2.5`) FROM newdate where location= 'Temple Way';
-SELECT AVG(`PM2.5`), AVG(`VPM2.5`) FROM newdate where location= 'Colston Avenue';
-SELECT AVG(`PM2.5`), AVG(`VPM2.5`) FROM newdate where location= 'Marlborough Street';
+INSERT INTO newtable select `Date Time`, `PM2.5`, `VPM2.5`, `location ID` FROM pollution_db2.readings where `Date Time` between '2019-01-01' and '2019-12-31';
+ALTER TABLE newtable
+MODIFY COLUMN `Date Time` DATETIME;
+SELECT `location id`, AVG(`PM2.5`), AVG(`VPM2.5`) FROM newtable where `location id`= 'AURN Bristol Centre'  AND date_format(`Date Time`,'%Y')='2019'AND date_format(`Date Time`,'%H')<='09'AND date_format(`Date Time`,'%H')>='07';
+SELECT AVG(`PM2.5`), AVG(`VPM2.5`) FROM newtable where `location id`= 'Brislington Depot'AND date_format(`Date Time`,'%Y')='2019'AND date_format(`Date Time`,'%H')<='09'AND date_format(`Date Time`,'%H')>='07';
+SELECT AVG(`PM2.5`), AVG(`VPM2.5`) FROM newtable where `location id`= 'Rupert Street'AND date_format(`Date Time`,'%Y')='2019'AND date_format(`Date Time`,'%H')<='09'AND date_format(`Date Time`,'%H')>='07';
+SELECT AVG(`PM2.5`), AVG(`VPM2.5`) FROM newtable where `location id`= 'IKEA M32'AND date_format(`Date Time`,'%Y')='2019'AND date_format(`Date Time`,'%H')<='09'AND date_format(`Date Time`,'%H')>='07';
+SELECT AVG(`PM2.5`), AVG(`VPM2.5`) FROM newtable where `location id`= 'Old Market'AND date_format(`Date Time`,'%Y')='2019'AND date_format(`Date Time`,'%H')<='09'AND date_format(`Date Time`,'%H')>='07';
+SELECT AVG(`PM2.5`), AVG(`VPM2.5`) FROM newtable where `location id`= 'Parson Street School'AND date_format(`Date Time`,'%Y')='2019'AND date_format(`Date Time`,'%H')<='09'AND date_format(`Date Time`,'%H')>='07';
+SELECT AVG(`PM2.5`), AVG(`VPM2.5`) FROM newtable where `location id`= 'Temple Meads Station'AND date_format(`Date Time`,'%Y')='2019'AND date_format(`Date Time`,'%H')<='09'AND date_format(`Date Time`,'%H')>='07';
+SELECT AVG(`PM2.5`), AVG(`VPM2.5`) FROM newtable where `location id`= 'Wells Road'AND date_format(`Date Time`,'%Y')='2019'AND date_format(`Date Time`,'%H')<='09'AND date_format(`Date Time`,'%H')>='07';
+SELECT AVG(`PM2.5`), AVG(`VPM2.5`) FROM newtable where `location id`= 'Trailer Portway P&R'AND date_format(`Date Time`,'%Y')='2019'AND date_format(`Date Time`,'%H')<='09'AND date_format(`Date Time`,'%H')>='07';
+SELECT AVG(`PM2.5`), AVG(`VPM2.5`) FROM newtable where `location id`= 'Newfoundland Road Police Station'AND date_format(`Date Time`,'%Y')='2019'AND date_format(`Date Time`,'%H')<='09'AND date_format(`Date Time`,'%H')>='07';
+SELECT AVG(`PM2.5`), AVG(`VPM2.5`) FROM newtable where `location id`= "Shiner's Garage"AND date_format(`Date Time`,'%Y')='2019'AND date_format(`Date Time`,'%H')<='09'AND date_format(`Date Time`,'%H')>='07';
+SELECT AVG(`PM2.5`), AVG(`VPM2.5`) FROM newtable where `location id`= 'AURN St Pauls'AND date_format(`Date Time`,'%Y')='2019'AND date_format(`Date Time`,'%H')<='09'AND date_format(`Date Time`,'%H')>='07';
+SELECT AVG(`PM2.5`), AVG(`VPM2.5`) FROM newtable where `location id`= 'Bath Road'AND date_format(`Date Time`,'%Y')='2019'AND date_format(`Date Time`,'%H')<='09'AND date_format(`Date Time`,'%H')>='07';
+SELECT AVG(`PM2.5`), AVG(`VPM2.5`) FROM newtable where `location id`= 'Cheltenham Road \ Station Road'AND date_format(`Date Time`,'%Y')='2019'AND date_format(`Date Time`,'%H')<='09'AND date_format(`Date Time`,'%H')>='07';
+SELECT AVG(`PM2.5`), AVG(`VPM2.5`) FROM newtable where `location id`= 'Fishponds Road'AND date_format(`Date Time`,'%Y')='2019'AND date_format(`Date Time`,'%H')<='09'AND date_format(`Date Time`,'%H')>='07';
+SELECT AVG(`PM2.5`), AVG(`VPM2.5`) FROM newtable where `location id`= 'CREATE Centre Roof'AND date_format(`Date Time`,'%Y')='2019'AND date_format(`Date Time`,'%H')<='09'AND date_format(`Date Time`,'%H')>='07';
+SELECT AVG(`PM2.5`), AVG(`VPM2.5`) FROM newtable where `location id`= 'Temple Way'AND date_format(`Date Time`,'%Y')='2019'AND date_format(`Date Time`,'%H')<='09'AND date_format(`Date Time`,'%H')>='07';
+SELECT AVG(`PM2.5`), AVG(`VPM2.5`) FROM newtable where `location id`= 'Colston Avenue'AND date_format(`Date Time`,'%Y')='2019'AND date_format(`Date Time`,'%H')<='09'AND date_format(`Date Time`,'%H')>='07';
+SELECT AVG(`PM2.5`), AVG(`VPM2.5`) FROM newtable where `location id`= 'Marlborough Street'AND date_format(`Date Time`,'%Y')='2019'AND date_format(`Date Time`,'%H')<='09'AND date_format(`Date Time`,'%H')>='07';
